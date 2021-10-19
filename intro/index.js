09a6170e8eccc6bc04b11c3cfd78c0c4a1be9656
Şule Dursun <sule.drsn98@gmail.com>
2021-10-20 02:42:44 +0300

    exporting different types

--- intro/index.js
+++ intro/index.js
@@ -1,4 +1,9 @@
-import {hello,topla} from "./my-module.js";//import multiple functions
+import merhaba, {topla, cikar, text, user, users} from "./my-module.js";//import multiple functions
+//export default should be separated and can be imported with different names
 
 console.log(topla(2,4));
-hello();
+console.log(cikar(4,2));
+console.log(text);
+console.log(user);
+console.log(users);
+merhaba("Ada");
--- intro/my-module.js
+++ intro/my-module.js
@@ -1,5 +1,22 @@
-export const hello = () => { //export separately
-    console.log("hello");
+const hello = name => { //if only one parameter
+    console.log(`hello ${name}`);//console.log("hello " + name) also for ` altGr + ;
 };
-export const topla = (a,b) => a+b; //oneline function
+const topla = (a,b) => a+b; //oneline function
+const cikar = (a,b) => a-b;
 
+
+const text = 'Text lorem ipsum';
+const user = {
+    name:"Kristen",
+    surname:"Stewart"
+};
+const users = [{
+    name:"Megan",
+    surname:"Fox"
+},{
+    name:"Ruby",
+    surname:"Rose"
+}];
+
+export {topla,cikar,text,user,users}; //export multiple funcs
+export default hello;
