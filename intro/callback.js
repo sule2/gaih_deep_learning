ec2c4cb09ac45993e7778588f811420d979cbe01
Şule Dursun <sule.drsn98@gmail.com>
2021-11-15 15:26:35 +0300

    Callback Functions/async-await

---
+++ intro/callback.js
@@ -0,0 +1,42 @@
+/*setTimeout(()=> {//similar to yield(C#)
+    console.log("Hello Mocha");
+},2000);*/
+
+ /*setInterval(()=> {//reminds me of Unity's update func
+    console.log("Belirtilen sürede bir tekrar çalışır.");
+ },1000);*/
+
+ /*const sayHi = (cb) =>{
+    cb();
+ };
+ sayHi(()=> {
+    console.log("Haweryu");
+ });*/
+import fetch from "node-fetch"; //data fetched from API
+/*fetch("https://jsonplaceholder.typicode.com/users").then(data => data.json()).then((users) => console.log(users));//data to json parsing
+fetch("https://jsonplaceholder.typicode.com/users").then(data => data.json()).then((users) => { //Fetch into fetch after the fetch has completed
+    console.log(users); //fetchception
+    fetch("https://jsonplaceholder.typicode.com/posts").then(data => data.json()).then((posts) => {
+        console.log(posts);
+        fetch("https://jsonplaceholder.typicode.com/posts/1").then(data => data.json()).then(post => console.log("Post 1 yüklendi",post));
+    });
+});//since js is async you have to put it inside to make it sync it literally needs to be fetchception to make it sync
+*/
+//with async-await we don't need to use fetchception
+async function getData() {
+    const users = await(await fetch("https://jsonplaceholder.typicode.com/users")).json();
+    const post = await(await fetch("https://jsonplaceholder.typicode.com/posts/1")).json();
+    console.log("Users loaded",users);
+    console.log("Post 1 loaded",post);
+}
+getData();
+/*
+const getData = async() => {//same thing
+    const resUsers = await fetch("https://jsonplaceholder.typicode.com/users");
+    const resPost = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const users = await resUsers.json();
+    const post = await resPost.json();
+
+    console.log("Kullanıcılar yüklendi",users);
+    console.log("Post 1 yüklendi",post);
+}; */
