c3c4ef976c1f02f8d6d7cc622bcd835f38c99034
Şule Dursun <sule.drsn98@gmail.com>
2021-11-17 11:51:32 +0300

    prop types

--- Lesson2_components/my_app/src/components/User.js
+++ Lesson2_components/my_app/src/components/User.js
@@ -1,14 +1,23 @@
+import propTypes from "prop-types";
 function User({name, surname, age, isLoggedIn, friends}){
     return(
         <div>
             <h1>{isLoggedIn ? `${name} ${surname} ${age} yaşında.` : "Giriş yapmadınız."}</h1>
             {
             friends.map((friend) => {
-                
+
                 return <div key={friend.id} >{friend.id}){friend.name}</div>
             })
             }
         </div>
     );
 }
+User.propTypes = {
+    name: propTypes.string,
+    surname: propTypes.string,
+    isLoggedIn: propTypes.bool,
+    age: propTypes.number,
+    friends: propTypes.array
+};
+
 export default User
