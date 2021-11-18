61c29a635a4142f71cbfd29c0b9fb535b42cd436
Şule Dursun <sule.drsn98@gmail.com>
2021-11-18 15:02:25 +0300

    filtering contacts

--- Lesson5_contacts-app/contacts-app/src/components/Contacts/List/index.js
+++ Lesson5_contacts-app/contacts-app/src/components/Contacts/List/index.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import {useState} from 'react'
 
 function List({contacts}) {
+    const[filterText,setFilterText] = useState('');
+    const filtered = contacts.filter((item) => {
+        return Object.keys(item).some((key) =>
+            item[key].toString().toLowerCase()
+            .includes(filterText.toLowerCase())
+        );
+    });
+    console.log(filtered);
     return (
         <div>
+            <input placeholder={"Filter Contact"} 
+            value={filterText} 
+            onChange={((e) => setFilterText(e.target.value))}/>
             <ul>
                 {
-                    contacts.map((contact,index)=> (
+                    filtered.map((contact,index)=> (
                         <li key={index}>{contact.fullname}</li>
                     ))
                 }
--- Lesson5_contacts-app/contacts-app/src/components/Contacts/index.js
+++ Lesson5_contacts-app/contacts-app/src/components/Contacts/index.js
@@ -2,7 +2,20 @@ import {useState, useEffect} from 'react'
 import List from './List';
 import Form from './Form';
 function Contacts() {
-    const [contacts,setContacts]=useState([]);
+    const [contacts,setContacts]=useState([
+        {
+            fullname: 'Mehmet',
+            phone_number: "532627"
+        },
+        {
+            fullname: 'Elijah',
+            phone_number: "526737"
+        },
+        {
+            fullname: 'Abel',
+            phone_number: "436217"
+        }
+    ]);
 
     useEffect(() => {
         console.log(contacts);
