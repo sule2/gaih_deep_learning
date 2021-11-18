e760315600afeb98a1f63a87e092f6668fdef145
Şule Dursun <sule.drsn98@gmail.com>
2021-11-18 14:27:06 +0300

    contacts form onSubmit

--- Lesson5_contacts-app/contacts-app/src/components/Contacts/Form/index.js
+++ Lesson5_contacts-app/contacts-app/src/components/Contacts/Form/index.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import {useState} from 'react'
 
-function Form() {
+const initialFormValues = {fullname:'',phone_number:''};
+
+function Form({addContact, contacts}) {
+    const [form,setForm] = useState(initialFormValues);
+
+    const onChangeInput = (e) =>{
+        setForm({...form,[e.target.name]: e.target.value});
+    };
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if(form.fullname===''||form.phone_number==='')
+        {
+            return false;
+        }
+        addContact([...contacts,form])
+        setForm(initialFormValues);
+    }
     return (
         <div>
-            Form
+            <form onSubmit={onSubmit}>
+            <div>
+            <input name="fullname" 
+            placeholder="Full Name" 
+            value={form.fullname}
+            onChange={onChangeInput}/>
+            </div>
+            <div>
+            <input name="phone_number" 
+            placeholder="Phone Number" 
+            value={form.phone_number}
+            onChange={onChangeInput}/>
+            </div>
+            <div>
+                <button>Add</button>
+            </div>
+            </form>
         </div>
     )
 }
