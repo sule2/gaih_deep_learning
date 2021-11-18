4a5d98fd4a9c155eb3fa72fe910bd46ab0f8c781
Şule Dursun <sule.drsn98@gmail.com>
2021-11-18 10:19:03 +0300

    useEffect hook

--- Lesson4_lifecycle/lifecycle/src/App.js
+++ Lesson4_lifecycle/lifecycle/src/App.js
@@ -1,10 +1,28 @@
-import {useState} from "react";
+import {useState,useEffect} from "react";
 
 function App() {
-  const [name,setName] = useState("Fatih");
+  const [number,setNumber] = useState(0);
+  const [name,setName] = useState("Mete");
+
+  useEffect(() => {
+    console.log("Component mount edildi.");
+  },[]);//dependency array is empty it'll catch the mounting moment
+
+  
+  useEffect(() => {
+    console.log("Number state güncellendi.");//effect
+  },[number]);
+  
+  useEffect(() => {
+    console.log("Name state güncellendi.");//effect
+  },[name]);
+  
   return (
     <div className="App">
+    <h1>{number}</h1>
+    <button onClick={() => setNumber(number + 1)}>Click</button>
     <h1>{name}</h1>
+    <button onClick={() => setName("Elijah")}>Click</button>
     </div>
   );
 }
