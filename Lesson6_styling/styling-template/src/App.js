f0f0b8bda439017366d4a01f6e4570f2e393cc8b
Şule Dursun <sule.drsn98@gmail.com>
2023-07-26 14:25:39 +0300

    removed node_modules, styling examples

---
+++ Lesson6_styling/styling-template/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import A from "./components/A";
+import B from './components/B';
+
+function App() {
+  return (
+    <div className="App" >
+      <header className="App-header" style={{backgroundColor:"cornflowerblue"}}>
+        <A/>
+        <B/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
