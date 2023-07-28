ef58f58550278195fc97b4105f28ce3392cfbd02
Şule Dursun <sule.drsn98@gmail.com>
2023-07-28 12:29:06 +0300

    converted link to navlink , added 404 Error page

---
+++ Lesson8_routing/routed-app/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>
+        <h2 style={{textAlign:"center",padding:"10rem",color:"crimson"}}>NO SUCH PAGE FOUND!</h2>
+    </div>
+  )
+}
+
+export default NotFound
--- Lesson8_routing/routed-app/src/components/navbar/NewRouter.js
+++ Lesson8_routing/routed-app/src/components/navbar/NewRouter.js
@@ -4,24 +4,52 @@ import {
     Routes,
     Route,
     Link,
+    NavLink
   } from "react-router-dom";
 import { Contact, Home } from './AppRouter';
 import About from '../About';
 import Companies from '../company/MyCompany';
+import styles from './styles.module.css';
+import NotFound from '../NotFound';
 
 function NewRouter() {
   return (
     <BrowserRouter>
-    <div className='bar-container' >
-          <Link className='bar-item' to="">HOME</Link>
-          <Link className='bar-item' to="about/" >ABOUT</Link>
-          <Link className='bar-item' to="contact/" >CONTACT</Link>
+    <div className={styles.barContainer} >
+      <NavLink
+        to="/"
+        className={({ isActive, isPending }) =>
+          isPending ? styles.pending : isActive ? styles.active  : styles.barItem
+        }
+      >
+        HOME
+      </NavLink>
+      <NavLink
+        to="about/"
+        className={({ isActive, isPending }) =>
+          isPending ? styles.pending : isActive ? styles.active : styles.barItem
+        }
+      >
+        ABOUT
+      </NavLink>
+      <NavLink
+        to="contact/"
+        className={({ isActive, isPending }) =>
+          isPending ? styles.pending : isActive ? styles.active : styles.barItem
+        }
+      >
+        CONTACT
+      </NavLink>
+          {/* <Link className={styles.barItem} to="">HOME</Link>
+          <Link className={styles.barItem} to="about/" >ABOUT</Link>
+          <Link className={styles.barItem} to="contact/" >CONTACT</Link> */}
     </div>
       <Routes>
         <Route path="/" element={<Home />} /> {/* v5 react-router-dom needs exact for '/' symbol */}
         <Route path="about/*" element={<About />} />
         <Route path="contact/*" element={<Contact />} />
         <Route path="companies/*" element={<Companies />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
