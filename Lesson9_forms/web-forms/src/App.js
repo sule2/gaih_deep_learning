95c2f3e5b46e19cf46463228f19e10552c57b368
Şule Dursun <sule.drsn98@gmail.com>
2023-07-28 16:22:24 +0300

    converted Formik to useFormik

--- Lesson9_forms/web-forms/src/App.js
+++ Lesson9_forms/web-forms/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Forms from './components/forms/Forms';
 import NewForm from './components/forms/NewForm';
+import NewForm1 from './components/forms/NewForm1';
 
 function App() {
   return (
     <div className="App">
-      <NewForm/>
+      <NewForm1/>
     </div>
   );
 }
